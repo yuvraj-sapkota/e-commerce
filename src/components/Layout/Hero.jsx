1f41844d07dacf1f6db12a574246a701eafe6f4e
yuvraj-sapkota <sharmayuvraj034@gmail.com>
2025-04-14 21:22:42 +0545

    updated hero section

--- src/components/Layout/Hero.jsx
+++ src/components/Layout/Hero.jsx
@@ -13,8 +13,8 @@ const Hero = () => {
        <div className="absolute inset-0 flex items-center justify-center bg-black/5 ">
         <div className="text-center  text-white p-6">
           <h1 className="uppercase text-4xl md:text-9xl font-bold -mb-4">Vacation <br /> Ready</h1>
-          <p className="text-sm tracking-tighter md:text-lg mb-6 p-6">Explore out vaction-ready outfits with fast world wide shipping </p>
-          <Link to="#" className="bg-white text-gray-950 px-6 py-2 rounded-sm text-lg"
+          <p className="text-sm tracking-tighter md:text-lg mb-6 p-6">Explore our vaction-ready outfits with fast world wide shipping </p>
+          <Link to="#" className="bg-white text-gray-950 px-6 py-2 rounded-sm text-lg hover:bg-red-500 hover:text-white"
           >Shop now</Link>
         </div>
        </div>
