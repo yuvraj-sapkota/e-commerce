ab43dd2370773ebb7ab5d475ccc172d79fdf770d
yuvraj-sapkota <sharmayuvraj034@gmail.com>
2025-04-14 21:20:37 +0545

    worked on hero image on hero page

--- src/components/Layout/Hero.jsx
+++ src/components/Layout/Hero.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import heroImage from "../../assets/heroImg.png";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <div className='bg-red-500'>Hero Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ipsum, accusamus illum dolores esse repellat! Nihil, vero sed temporibus assumenda nulla odit vel fuga non a laborum rem error qui quibusdam commodi rerum molestias. Recusandae reprehenderit voluptate id alias voluptatibus velit eveniet harum pariatur amet ipsam a iste omnis atque, impedit vitae eos porro ex ad mollitia ab ducimus repellendus? Laboriosam maxime error eveniet adipisci ipsam dignissimos molestiae ut nihil? Officiis possimus aspernatur impedit ab voluptas. Eum, adipisci sapiente assumenda dolorem sunt est et delectus voluptas nemo quasi pariatur ipsam nobis, ea ad iusto veniam. Soluta vitae optio fugit eum.</div>
-  )
-}
+    <>
+      <section className="relative">
+       <div className="md:h-[600px] lg:h-[750px] h-[400px]   overflow-hidden mx-auto">
+       <img src={heroImage} alt=""  className="h-[100%] w-[100%] overflow-hidden object-cover object-top mx-auto"/>
+       </div>
 
-export default Hero
+       <div className="absolute inset-0 flex items-center justify-center bg-black/5 ">
+        <div className="text-center  text-white p-6">
+          <h1 className="uppercase text-4xl md:text-9xl font-bold -mb-4">Vacation <br /> Ready</h1>
+          <p className="text-sm tracking-tighter md:text-lg mb-6 p-6">Explore out vaction-ready outfits with fast world wide shipping </p>
+          <Link to="#" className="bg-white text-gray-950 px-6 py-2 rounded-sm text-lg"
+          >Shop now</Link>
+        </div>
+       </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
