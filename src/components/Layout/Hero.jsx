f09d6c4003133c7bc26e1f45f13efbc343bddd83
yuvraj-sapkota <sharmayuvraj034@gmail.com>
2025-04-14 21:23:55 +0545

    updated hero section

--- src/components/Layout/Hero.jsx
+++ src/components/Layout/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
           <h1 className="uppercase text-4xl md:text-9xl font-bold -mb-4">Vacation <br /> Ready</h1>
           <p className="text-sm tracking-tighter md:text-lg mb-6 p-6">Explore our vaction-ready outfits with fast world wide shipping </p>
           <Link to="#" className="bg-white text-gray-950 px-6 py-2 rounded-sm text-lg hover:bg-red-500 hover:text-white"
-          >Shop now</Link>
+          >Shops now</Link>
         </div>
        </div>
       </section>
