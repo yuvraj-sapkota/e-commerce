53f820024ca79febd18914ab1347690e0b974b7d
yuvraj-sapkota <sharmayuvraj034@gmail.com>
2025-04-14 21:24:42 +0545

    updated hero section

--- src/components/Layout/Hero.jsx
+++ src/components/Layout/Hero.jsx
@@ -6,18 +6,30 @@ const Hero = () => {
   return (
     <>
       <section className="relative">
-       <div className="md:h-[600px] lg:h-[750px] h-[400px]   overflow-hidden mx-auto">
-       <img src={heroImage} alt=""  className="h-[100%] w-[100%] overflow-hidden object-cover object-top mx-auto"/>
-       </div>
-
-       <div className="absolute inset-0 flex items-center justify-center bg-black/5 ">
-        <div className="text-center  text-white p-6">
-          <h1 className="uppercase text-4xl md:text-9xl font-bold -mb-4">Vacation <br /> Ready</h1>
-          <p className="text-sm tracking-tighter md:text-lg mb-6 p-6">Explore our vaction-ready outfits with fast world wide shipping </p>
-          <Link to="#" className="bg-white text-gray-950 px-6 py-2 rounded-sm text-lg hover:bg-red-500 hover:text-white"
-          >Shops now</Link>
+        <div className="md:h-[600px] lg:h-[750px] h-[400px]   overflow-hidden mx-auto">
+          <img
+            src={heroImage}
+            alt=""
+            className="h-[100%] w-[100%] overflow-hidden object-cover object-top mx-auto"
+          />
         </div>
-       </div>
+
+        <div className="absolute inset-0 flex items-center justify-center bg-black/5 ">
+          <div className="text-center  text-white p-6">
+            <h1 className="uppercase text-4xl md:text-9xl font-bold -mb-4">
+              Vacation <br /> Ready
+            </h1>
+            <p className="text-sm tracking-tighter md:text-lg mb-6 p-6">
+              Explore our vaction-ready outfits with fast world wide shipping{" "}
+            </p>
+            <Link
+              to="#"
+              className="bg-white text-gray-950 px-6 py-2 rounded-sm text-lg hover:bg-red-500 hover:text-white"
+            >
+              Shop now
+            </Link>
+          </div>
+        </div>
       </section>
     </>
   );
