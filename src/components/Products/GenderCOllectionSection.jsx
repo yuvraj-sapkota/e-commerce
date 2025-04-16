ee7ba5a0823173b25a771c6a62c94455da7f1ef4
yuvraj-sapkota <sharmayuvraj034@gmail.com>
2025-04-16 20:34:24 +0545

    worked on gender's collection

--- src/components/Products/GenderCOllectionSection.jsx
+++ src/components/Products/GenderCOllectionSection.jsx
@@ -1,5 +1,6 @@
 import menscollection from "../../assets/menscollectionImage.jpg";
 import girlscollectionImage from "../../assets/girlscollectionImage.jpg";
+import { Link } from "react-router-dom";
 
 const GenderCOllectionSection = () => {
   return (
@@ -21,8 +22,49 @@ const GenderCOllectionSection = () => {
         </div>
       </div> */}
 
-      <div className="py-16">
+      <div className="py-16 px-4">
+        <div className="container mx-auto flex flex-col md:flex-row gap-8">
+          {/* women's colelction  */}
+          <div className="relative">
+            <img
+              src={girlscollectionImage}
+              alt="girls collection"
+              className="w-full h-[400px] object-cover"
+            />
 
+            <div className="absolute bottom-8 left-8 bg-white  p-2">
+              <h2 className="text-2xl font-bold text-gray-900 mb-3">
+                Women's Collection
+              </h2>
+              <Link
+                to="/collections/all?gender=Women "
+                className="text-gray-900 underline"
+              >
+                Show Now
+              </Link>
+            </div>
+          </div>
+          {/* men's colelction  */}
+          <div className="relative">
+            <img
+              src={menscollection}
+              alt="mens collection"
+              className="w-full h-[400px] object-cover"
+            />
+
+            <div className="absolute bottom-8 left-8 bg-white  p-2">
+              <h2 className="text-2xl font-bold text-gray-900 mb-3">
+                Men's Collection
+              </h2>
+              <Link
+                to="/collections/all?gender=men "
+                className="text-gray-900 underline"
+              >
+                Show Now
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
