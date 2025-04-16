e52a323fc4ecdc8cf9ed4f31d98d2b9c1e0ec012
yuvraj-sapkota <sharmayuvraj034@gmail.com>
2025-04-16 21:07:17 +0545

    working on the new Arrivals section

--- src/components/Products/NewArrivals.jsx
+++ src/components/Products/NewArrivals.jsx
@@ -1,3 +1,6 @@
+import { CiCircleChevLeft } from "react-icons/ci";
+import { CiCircleChevRight } from "react-icons/ci";
+import { Link } from "react-router-dom";
 const NewArrivals = () => {
   const newArrivals = [
     {
@@ -97,9 +100,30 @@ const NewArrivals = () => {
         {/* scroll button  */}
         <div className="absolute right-0 bottom-[-30px] flex space-x-2 ">
           <button className="p-2 rounded border bg-white text-black">
-            <FiChevronLeft />
+            <CiCircleChevLeft className="text-2xl" />
+          </button>
+          <button className="p-2 rounded border bg-white text-black">
+            <CiCircleChevRight className="text-2xl" />
           </button>
         </div>
+      </div>
+
+      {/* Scrollable content  */}
+      <div className="container mx-auto overflow-scroll flex space-x-6 relative">
+        {newArrivals.map((product) => (
+          <div key={product.id}>
+            <img
+              src={product.images[0]?.url}
+              alt={product.images[0]?.altText}
+            />
+            <div className="absolute bottom-0 left-0 right-0 backdrop-blur-md backdrop-opacity-50 text-white p-4 rounded-b-lg">
+              <Link to={`/porduct/${product._id}`} className="block">
+                <h4 className="font-medium">{product.name}</h4>
+                <p className="mt-1">${product.price}</p>
+              </Link>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
